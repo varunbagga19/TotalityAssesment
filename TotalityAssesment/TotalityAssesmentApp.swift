39dd725d82e8785b4bf899bd78a11405f9b7fae4
varunbagga19 <varunbagga19@gmail.com>
2023-09-27 17:57:19 +0530

    Initial Commit

---
+++ TotalityAssesment/TotalityAssesmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  TotalityAssesmentApp.swift
+//  TotalityAssesment
+//
+//  Created by Varun Bagga on 27/09/23.
+//
+
+import SwiftUI
+
+@main
+struct TotalityAssesmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
